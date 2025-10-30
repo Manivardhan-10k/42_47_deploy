aa0c9dec9177bfb6262bf09cee31b2b77494a652
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-10-30 12:50:34 +0530

    first deploy of django

---
+++ dep_pro/dep_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django .http  import HttpResponse
+
+# Create your views here.
+
+
+
+def welcome(req):
+    return HttpResponse ("welcome to mani's app from render")
