6b23a21bc28ec6a7b2a862e7060713cd62856886
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-10-31 12:30:14 +0530

    created new endpoint

--- django_pro/django_app/views.py
+++ django_pro/django_app/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def welcome(req):
-    return HttpResponse("welcome to mani's app from render")
+    return HttpResponse("welcome to mani's app from render") 
+
+def  sample(req):
+    return JsonResponse ({"msg":"json response from render"})
