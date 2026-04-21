8050eea0c2b5a4fa2897b8f4c4ac13f16cb6faab
Manivardhan <159030573+Manivardhan-10k@users.noreply.github.com>
2026-04-21 13:11:20 +0530

    Comment out user-related URL paths
    Comment out user-related URL paths for future reference.

--- django_pro/django_app/urls.py
+++ django_pro/django_app/urls.py
@@ -7,13 +7,13 @@ from . import views
 urlpatterns = [
     path('', views.welcome),
     path('sample/', views.sample),
-    path('register/', views.reg_user),
-    path('users/', views.get_users),
-    path('user/<int:id>/', views.get_user_by_id),
-    path('update/<int:id>/', views.update_user),
-    path('delete/<int:id>/', views.delete_user),
-    path("login/",view=views.login_user),
-    path("send_file/",view=views.send_file)
+    # path('register/', views.reg_user),
+    # path('users/', views.get_users),
+    # path('user/<int:id>/', views.get_user_by_id),
+    # path('update/<int:id>/', views.update_user),
+    # path('delete/<int:id>/', views.delete_user),
+    # path("login/",view=views.login_user),
+    # path("send_file/",view=views.send_file)
 
 
     # path("",view=views.sample_template,name="default"),
